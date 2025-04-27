cb483e628600aed07d4549d9a7c7f6463f5c69d9
Mishu <152959369+Suromishu@users.noreply.github.com>
2025-04-27 16:47:22 +0800

    Initial commit

---
+++ src/ClassRoomExercise/demo03.java
@@ -0,0 +1,47 @@
+package ClassRoomExercise;
+
+import OtherCode.PerTools.FlexibleRead;
+
+import java.util.InputMismatchException;
+
+public class demo03 {
+    public static void main(String[] args) {
+        exceptTest();
+    }
+
+    public static void exceptTest(){
+
+        int b = 0;
+
+        try {
+            int a = FlexibleRead.readInt();
+
+            b = 100 / a;
+            System.out.println(b);
+
+//            System.out.println("abc12345".charAt(a));
+
+        } catch (InputMismatchException e) {
+            System.out.println("录入不匹配");
+        } catch (ArithmeticException e) {
+            System.out.println("除数不能为0");
+        } catch (StringIndexOutOfBoundsException e) {
+            System.out.println("索引越界");
+        } finally {
+            System.out.println("不论是否有异常，都会执行");
+        }
+
+        System.out.println("程序继续执行");
+
+        try {
+            int c;
+            if (b > 1)
+                throw new RuntimeException("b大于1");
+            else
+                c = b + 100;
+            System.out.println(c);
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
